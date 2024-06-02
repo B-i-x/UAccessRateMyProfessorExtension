fc0abd55e8f7613e144f709057024d632878c257
B-i-x <bix.romero@gmail.com>
2024-06-02 12:39:38 -0700

    cors error

--- content.js
+++ content.js
@@ -1,12 +1,19 @@
 async function fetchProfessorData() {
-    const response = await fetch('https://www.ratemyprofessors.com/search/professors/1402?q=test');
-    if (!response.ok) {
-      alert('Error fetching data: ' + response.statusText);
-      return;
-    }
-    const data = await response.text();
-    alert('Response from Rate My Professors: ' + data);
-}
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = 'https://www.ratemyprofessors.com/search/professors/1402?q=test';
   
-fetchProfessorData();
+    try {
+      const response = await fetch(proxyUrl + targetUrl);
+      if (!response.ok) {
+        alert('Error fetching data: ' + response.statusText);
+        return;
+      }
+      const data = await response.text();
+      alert('Response from Rate My Professors: ' + data);
+    } catch (error) {
+      alert('Failed to fetch data: ' + error.message);
+    }
+  }
+  
+  fetchProfessorData();
   
